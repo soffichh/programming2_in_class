5f439273bdbb07bd1c4b16ebbaa22957eaf1b258
soffichh <35131626+soffichh@users.noreply.github.com>
2018-03-01 11:20:55 +0200

    Add files via upload

---
+++ lll/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <list>
+
+using namespace std;
+void nums();
+{
+    list<int n>numbers;
+    numbers.push_front(6);
+    numbers.push_front(4);
+    numbers.push_back(1);
+    numbers.push_back(2);
+
+}
+int main()
+{
+
+}
